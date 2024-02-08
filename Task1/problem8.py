7f38e0ca69a0494092a4e25f34d2f88ac531bf53
Anis Emad <anisemad123@gmail.com>
2024-02-08 18:57:35 +0200

    problem8

---
+++ Task1/problem8.py
@@ -0,0 +1,12 @@
+#task 1 problem 8
+
+num = int(input("Enter a number: "))
+
+sum = 0
+for i in range(1, num):
+    if num % i == 0:
+        sum += i
+if sum == num:
+    print(f"{num} is a perfect number")
+else:
+    print(f"{num} is no a perfect number")
