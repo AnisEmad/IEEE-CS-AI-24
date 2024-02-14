812e55ae87c1e3f10ec14ddce985a12a6c574fb1
Anis Emad <anisemad123@gmail.com>
2024-02-14 14:20:53 +0200

    added tests

--- Task2/problem1.py
+++ Task2/problem1.py
@@ -1,9 +1,25 @@
+def test_type(year):
+    while True:
+        try:
+            year = int(year)
+            break
+        except:
+            year = input("Please enter an intger number: ")
+    return year
+def test_signal(year):
+    if year <= 0:
+        year = input("Please enter a poistive number: ")
+    return test_type(year)
+def check(year):
+    year = test_type(year)
+    year = test_signal(year)
+    return year
 def is_leap(year):
     leap = False
-
+    year = check(year)
     if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
         leap = True
     return leap
 
-year = int(input())
+year = input("Enter a year: ")
 print(is_leap(year))
