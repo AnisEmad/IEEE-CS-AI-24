a4a754cec4a819efde53dc0c539a8a5e5477a179
Anis Emad <anisemad123@gmail.com>
2024-02-17 21:56:02 +0200

    problem5

---
+++ Task2/problem5.py
@@ -0,0 +1,17 @@
+library = {
+    "book1" : {
+        "title": "atomic habits",
+        "author": "james clear",
+        "publication year": 2002
+    },
+    "book2" : {
+        "title": "how to be great",
+        "author": "Om Ali",
+        "publicatoin year" : 2025
+    },
+    "book3" : {
+        "title": "the history of lol",
+        "author": "abyusif",
+        "publicatoin year": 2017
+    }
+}
